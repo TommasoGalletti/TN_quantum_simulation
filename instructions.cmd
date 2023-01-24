09934b8a3ad3f98f8e377524a549b69795d0a949
Claudio Galletti <claudiog64@gmail.com>
2023-01-24 22:00:29 +0100

    Setup instructions for full env

--- instructions.cmd
+++ instructions.cmd
@@ -1,19 +1,20 @@
-Instructions:
+REM Setup instructions:
+
+pip install qibo
 
 pip install opt_einsum
 pip install autoray
 pip install networkx
 
+pip install qimb
 
 
-# Running in Windows
-"C:\Program Files\Python310\python.exe" "c:\Users\tommy\OneDrive\Documenti\Python Scripts\example-circuit-1.py"
-"C:\Program Files\Python310\python.exe" "c:\Users\tommy\OneDrive\Documenti\Python Scripts\example-circuit-2.py"
+REM Running in Windows
 
 python "c:\Users\tommy\OneDrive\Documenti\Python Scripts\example-circuit-1.py"
-
 python "c:/Users/tommy/OneDrive/Documenti/GitHub/TN_quantum_simulation/quimb - bell state.py"
 
 
-# in Ubuntu (WSL)
+REM in Ubuntu (WSL)
+
 python3 "/mnt/c/Users/tommy/OneDrive/Documenti/Python Scripts/example-circuit-1.py"
