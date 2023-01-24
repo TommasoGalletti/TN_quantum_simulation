efee0f14ef3d7f8cd5dfd66ab4a08c6cf4574d49
TommasoGalletti <75483857+TommasoGalletti@users.noreply.github.com>
2023-01-24 20:42:29 +0100

    1° disegno TN (bell state + Rz)

--- quimb - bell state.py
+++ quimb - bell state.py
@@ -6,6 +6,6 @@ circ = qtn.Circuit(N)
 
 circ.apply_gate('H', 0)
 circ.apply_gate('CNOT', 0, 1)
-circ.apply_gate('RZ', 1.57)
+circ.apply_gate('RZ', 1.57, 1)
 
 circ.psi.draw(color=['H', 'CNOT', 'RZ'])
