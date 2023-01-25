27a2e68ca4ed4ef98fce903062f122e9b8b777c6
Claudio Galletti <claudiog64@gmail.com>
2023-01-25 23:06:13 +0100

    Minor doc editing

--- instructions.cmd
+++ instructions.cmd
@@ -2,11 +2,54 @@ REM Setup instructions:
 
 pip install qibo
 
+pip install numpy
+pip install scipy
+pip install numba
+pip install cytoolz
+pip install tqdm
+pip install psutil
+
 pip install opt_einsum
 pip install autoray
 pip install networkx
 
 pip install qimb
+
+REM pip list
+REM Package            Version
+REM ------------------ -------
+REM autoray            0.5.3
+REM cma                3.2.2
+REM colorama           0.4.6
+REM contourpy          1.0.7
+REM cycler             0.11.0
+REM cytoolz            0.12.1
+REM fonttools          4.38.0
+REM importlib-metadata 6.0.0
+REM joblib             1.2.0
+REM kiwisolver         1.4.4
+REM llvmlite           0.39.1
+REM matplotlib         3.6.3
+REM mpmath             1.2.1
+REM networkx           3.0
+REM numba              0.56.4
+REM numpy              1.23.5
+REM opt-einsum         3.3.0
+REM packaging          23.0
+REM Pillow             9.4.0
+REM psutil             5.9.4
+REM pyparsing          3.0.9
+REM python-dateutil    2.8.2
+REM PyYAML             6.0
+REM qibo               0.1.10
+REM quimb              1.4.2
+REM scipy              1.10.0
+REM six                1.16.0
+REM sympy              1.11.1
+REM tabulate           0.9.0
+REM toolz              0.12.0
+REM tqdm               4.64.1
+REM zipp               3.11.0
 
 
 REM Running in Windows
