2c6308b46813b3fe3ab989748c0a4971bb20ab0a
Claudio Galletti <claudiog64@gmail.com>
2023-01-25 23:05:20 +0100

    Use apply_gates, fix for CX and psi mispelled as psy

--- quimb - q full adder.py
+++ quimb - q full adder.py
@@ -4,10 +4,20 @@ import quimb.tensor as qtn
 N = 4
 circ = qtn.Circuit(N)
 
-circ.apply_gate('ccX', 0, 1, 3) # <--- errore 
-circ.apply_gate('CNOT', 0, 1)
-circ.apply_gate('ccX', 1, 2, 3)
-circ.apply_gate('CNOT', 1, 2)
-circ.apply_gate('CNOT', 0, 1)
+gates = [
+            ('CX', 0, 1, 3),
+            ('CNOT', 0, 1),
+            ('CX', 1, 2, 3),
+            ('CNOT', 1, 2),
+            ('CNOT', 0, 1)
+        ]
 
-circ.psy.draw(color=['ccX', 'CNOT'])
+circ.apply_gates(gates)
+
+#circ.apply_gate('ccX', 0, 1, 3) # <--- errore 
+#circ.apply_gate('CNOT', 0, 1)
+#circ.apply_gate('ccX', 1, 2, 3)
+#circ.apply_gate('CNOT', 1, 2)
+#circ.apply_gate('CNOT', 0, 1)
+
+circ.psi.draw(color=['CX', 'CNOT'])
