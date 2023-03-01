b6156989e2a36b10179ddf68498229ee52edd134
TommasoGalletti <75483857+TommasoGalletti@users.noreply.github.com>
2023-03-01 18:48:49 +0100

    added descriptions for each step

--- qu - HiddenShift.py
+++ qu - HiddenShift.py
@@ -9,33 +9,33 @@ regs = list(range(N))
 
 circ = qtn.Circuit(N)
 
-def oracle(self):                                           #Implement function {f(x) = Σ_i x_(2i) x_(2i+1)}.
-    for i in range(N // 2):
+def oracle(self):                                           #Implement function {f(x) = Σ_i x_(2i) x_(2i+1)} as a series of CZ gates
+    for i in range(N // 2):                                 #oracle f
         circ.apply_gate('CZ', regs[2*i], regs[2*i + 1])
 
-#Hadamard
+#Hadamard (superposition - they act as a QFT)
 for i in range(N):
     circ.apply_gate('H', regs[i])
 
-for i, ish in enumerate(self.shift):    #cos'è self.shift?
+for i, ish in enumerate(self.shift):    #apply shift (|x> -> X|x>)
     if ish:
         circ.apply_gate('X', regs[i])
 
-for gate in self.oracle():              #che fa?
-    circ.apply_gate('gate', )
+for gate in self.oracle():              #query oracle f
+        circ.apply_gate('gate', )
 
-for i, ish in enumerate(self.shift):    #cos'è self.shift?
+for i, ish in enumerate(self.shift):    #apply shift (recover the |x> states)
     if ish:
         circ.apply_gate('X', regs[i])
 
-#Hadamard
+#Hadamard (fourier transform to generate superposition with an extra phase added to f(x+s))
 for i in range(N):
     circ.apply_gate('H', regs[i])
 
-for gate in self.oracle():              #..?
+for gate in self.oracle():              #query oracle f (this simplifies the phase)
     circ.apply_gate('gate', )
 
-#Hadamard
+#Hadamard (inverse fourier transform -> go back to the shift state |s>)
 for i in range(N):
     circ.apply_gate('H', regs[i])
 
