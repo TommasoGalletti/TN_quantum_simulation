f5ea90f9500ba05a6fc1b9401302195e0c6ac8d4
TommasoGalletti <75483857+TommasoGalletti@users.noreply.github.com>
2023-03-06 18:38:16 +0100

    QFT: plotted time(# of qubits)

--- qu - QFT.py
+++ qu - QFT.py
@@ -1,32 +1,70 @@
-import numpy as np                  #FA TUTTO QUELLO CHE DEVE, FORSE PROBLEMA CON SWAPs
+import numpy as np
+import time
+import timeit
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 import quimb as qu
 import quimb.tensor as qtn
 
-# Number of qubits
-N = 80                                                          #tested up to 80 qubits 
+bigN = 10
+totaltimevec = list()
+processtimevec = list()
+nvec = list()
 
-#registro ordine qubit
-regs = list(range(N))
+for n in range(bigN):
+    # Number of qubits
+    nvec.append(n + 1)
+    N = n + 1
 
-#create circuit
-circ = qtn.Circuit(N)
+    t0 = timeit.default_timer() #t_0
+    tprocess0 = time.process_time()
 
-for i in range(N):
-    circ.apply_gate('H', regs[i])                               #first we apply an Hadamard gate to the i-th qb
+    #registro ordine qubit
+    regs = list(range(N))
 
-    for j in range(i + 1, N):
-        theta = np.pi / 2 ** (j - i)                            #calculating theta angle, which we feed as a parameter to the CU1 gate
-        circ.apply_gate('CU1', theta, regs[i], regs[j])         #we apply a controlled unitary (1) gate to gate i and all gates below i
-        
-for i in range(N // 2):
-    circ.apply_gate('SWAP', regs[i], regs[N - i - 1])           #swap gates
+    #create circuit
+    circ = qtn.Circuit(N)
 
-for b in circ.sample(3):                                        #sample results (3 times)
-    print(b)
+    for i in range(N):
+        circ.apply_gate('H', regs[i])                               #first we apply an Hadamard gate to the i-th qb
 
-circ.psi.draw(color=['H', 'CU1', 'SWAP'])                       #circuit drawing - focus on gate types
-circ.psi.draw(color=[f'I{i}' for i in range(N)])                #circuit drawing - focus on qubit paths
+        for j in range(i + 1, N):
+            theta = np.pi / 2 ** (j - i)                            #calculating theta angle, which we feed as a parameter to the CU1 gate
+            circ.apply_gate('CU1', theta, regs[i], regs[j])         #we apply a controlled unitary (1) gate to gate i and all gates below i
+            
+    for i in range(N // 2):
+        circ.apply_gate('SWAP', regs[i], regs[N - i - 1])           #swap gates
+
+    for b in circ.sample(1):                                        #sample results (1 time)
+        print(b)
+
+    t1 = timeit.default_timer()    #t_1
+    tprocess1 = time.process_time()
+
+    totaltimevec.append(t1 - t0)   #total time elapsed
+    processtimevec.append(tprocess1 - tprocess0)
+
+fig1, ax1 = plt.subplots()
+ax1.plot(nvec, totaltimevec, 'ro')
+ax1.set_title('total time')
+ax1.set_ylabel('time [s]')
+ax1.set_xlabel('# of qubits')
+ax1.grid()
+
+plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/test_total_time_QFT.pdf")
+
+fig2, ax2 = plt.subplots()
+ax2.plot(nvec, processtimevec, 'ro')
+ax2.set_title('CPU time')
+ax2.set_ylabel('time [s]')
+ax2.set_xlabel('# of qubits')
+ax2.grid()
+
+plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/test_process_time_QFT.pdf")
+
+"""circ.psi.draw(color=['H', 'CU1', 'SWAP'])                       #circuit drawing - focus on gate types
+circ.psi.draw(color=[f'I{i}' for i in range(N)])                #circuit drawing - focus on qubit paths"""
 
 
 #for b in range(2**N):
