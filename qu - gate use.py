82eb24d4826700336ac9bfaa29da285582cd2bd8
TommasoGalletti <75483857+TommasoGalletti@users.noreply.github.com>
2023-03-10 16:54:44 +0100

    Deleted old files, updated quimb > gate use

--- qu - gate use.py
+++ qu - gate use.py
@@ -1,8 +1,20 @@
 import quimb as qu
 import quimb.tensor as qtn
+import numpy
+import math
 
-N = 4
+N = 2
 circ = qtn.Circuit(N)
+
+circ.apply_gate('H', 0)
+circ.apply_gate('CNOT', 0, 1)
+circ.apply_gate('RZ', 1.57, 1)
+
+circ.apply_gate('RY', 2 * numpy.arccos(1 / math.sqrt(3)), 1)
+#controlled Hadamard between 0, 1
+circ.apply_gate('CNOT', 1, 2)
+circ.apply_gate('CNOT', 0, 1)
+circ.apply_gate('X', 0)             #PAULI X: NOT
 
 gates = [
             ('CX', 0, 1, 3),
@@ -20,4 +32,6 @@ circ.apply_gates(gates)
 #circ.apply_gate('CNOT', 1, 2)
 #circ.apply_gate('CNOT', 0, 1)
 
-circ.psi.draw(color=['CX', 'CNOT'])
+#circ.psi.draw(color=['CX', 'CNOT'])
+
+#circ.psi.draw(color=['H', 'CNOT', 'RZ'])
