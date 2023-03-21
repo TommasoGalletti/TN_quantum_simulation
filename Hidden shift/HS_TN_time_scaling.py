59f58bf8ab2731631209f17364b6f1221485e735
TommasoGalletti <75483857+TommasoGalletti@users.noreply.github.com>
2023-03-21 22:11:58 +0100

    Split time scaling and output analysis

--- Hidden shift/HS_TN_time_scaling.py
+++ Hidden shift/HS_TN_time_scaling.py
@@ -4,7 +4,6 @@ import timeit
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import random
-import seaborn as sns
 
 import quimb as qu
 import quimb.tensor as qtn
@@ -36,7 +35,6 @@ def build_HS(N, regs, shift):
 
 maxqubit = 3       #37
 ntimes = 10^2     #1000
-nsampling = 10^3       #100k ?
 
 meantotaltime = np.zeros(maxqubit, np.float32)
 totaltimeerror = np.zeros(maxqubit, np.float32)
@@ -48,8 +46,9 @@ for n in range(maxqubit):
     singletotaltime = np.zeros(ntimes, np.float32)
     singleprocesstime = np.zeros(ntimes, np.float32)
 
+    N = n + 1
+
     for i in range(ntimes):
-        N = n + 1
 
         regs = list(range(N))
         circ = qtn.Circuit(N)
@@ -70,34 +69,12 @@ for n in range(maxqubit):
         singletotaltime[i] = ttot
         singleprocesstime[i] = tprocess
 
-    """for b in circ.sample(nsampling):         #print (sample string)_N_qbmax_1
-            for a in range(maxqubit):
-                bigmatrix[:,a] = b[a]"""
-
     meantotaltime[n] = np.mean(singletotaltime)
     totaltimeerror[n] = np.std(singletotaltime)
 
     meanprocesstime[n] = np.mean(singleprocesstime)
     processtimeerror[n] = np.mean(singleprocesstime)
 
-bigmatrix = np.zeros((nsampling, maxqubit), np.int8)
-
-for c in range(nsampling):    
-    for a in range(maxqubit):       #serve questo for o si può fare in altro modo?
-        for b in circ.sample(nsampling):       
-            bigmatrix[c,a] = b[a]
-
-#print(bigmatrix)   
-
-farray = np.sum(bigmatrix, axis = 0) / nsampling
-#print(farray)
-
-rij = np.corrcoef(bigmatrix, rowvar= False)
-#print(rij)
-
-#frequency histogram
-histo = plt.bar(farray, width= np.arange(maxqubit))
-plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/HS/HS_frequency_histo.pdf")
 
 #total time
 fig2 = plt.figure()
@@ -122,15 +99,3 @@ fig4.supxlabel('# of qubits')
 fig4.supylabel('time [s]')
 #plt.legend(loc='upper left')
 plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/HS/HS_CPU_time_error.pdf")
-
-#Correlation heatmap
-mask = np.triu(np.ones_like(rij, dtype=bool))
-cmap = sns.diverging_palette(230, 20, as_cmap=True)
-f, ax = plt.subplots(figsize=(maxqubit,maxqubit))
-heatmap = sns.heatmap(rij, mask=mask, cmap=cmap, vmax=.3, center=0,
-            square=True, linewidths=.5, cbar_kws={"shrink": .5})
-f.suptitle('Qubit result correlation')
-f.supxlabel('Qubit #')
-f.supylabel('Qubit #')
-plt.legend('Cij = COV(X_i,X_j)/(VAR(X_i)*VAR(X_j))^0.5', loc="upper right")
-plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/HS/HS_correlation_heatmap.pdf")
