time scaling running, fixing sampling errors

QFT/QFT_TN_vs_SV.py
@@ -8,32 +8,77 @@ import sys  #robe da riga di comando
 
 maxqubit = 33
 
+tnfarrays = []
+tnrijs = []
 
+with open('/TN_samples.txt', mode='r') as file:
+    for line in file:
+        line = line.strip()  # remove leading/trailing white space
+        if not line:         # skip empty lines
+            continue
+        # split line into farray and rij using comma as delimiter
+        farray, rij = line.split(',')
+        # convert farray and rij strings to numpy arrays
+        farray = np.fromstring(farray, sep=',')
+        rij = np.fromstring(rij, sep=',').reshape((len(farray), len(farray)))
+        # add farray and rij to respective lists
+        tnfarrays.append(farray)
+        tnrijs.append(rij)
+
+svfarrays = []
+svrijs = []
+
+with open('/SV_samples.txt', mode='r') as file:
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+
+        farray, rij = line.split(',')
+
+        farray = np.fromstring(farray, sep=',')
+        rij = np.fromstring(rij, sep=',').reshape((len(farray), len(farray)))
+
+        svfarrays.append(farray)
+        svrijs.append(rij)
+
+
+
+"""
 sv_data = np.loadtxt('SV_samples.txt', delimiter=',')
 sv_farray = sv_data[0]
 sv_r = sv_data[1:]
 
 tn_data = np.loadtxt('TN_samples.txt', delimiter=',')
 tn_farray = tn_data[0]
-tn_r = tn_data[1:]
+tn_r = tn_data[1:]"""
 
 
-#distanza euclidea
-fdist = np.linalg.norm(tn_farray - sv_farray)
+#distanza euclidea - SI
+fdist = np.zeros(maxqubit, np.float64)
+for d in range(maxqubit):
+    fdist[d] = np.linalg.norm(tnfarrays[d] - svfarrays[d])
 
-#pearson correlation (?)
-pearsoncorr = np.corrcoef(tn_farray, sv_farray)
+#pearson correlation - NO
+pearsoncorr = np.zeros(maxqubit, np.float64)
+for p in range(maxqubit):
+    pearsoncorr[p] = np.corrcoef(tnfarrays[p], svfarrays[p])
+
 
 #wilcoxon-mann-whitney test
 #test di Kolmogorov-Smirnov
 #test t di Student?
 
+#coefficiente di correlazione di Spearman o Kendall
+
 
 #Frobenius norm della matrice differenza
-corrdist = np.linalg.norm(tn_r - sv_r)
+corrdist = np.zeros(maxqubit, np.float64)
+for d in range(maxqubit):
+    corrdist[d] = np.linalg.norm(tnrijs[d] - svrijs[d])
 
 
-
+"""
 #TN frequency histogram
 histo = plt.bar(np.arange(maxqubit), tn_farray)
 plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/QFT/QFT_TN_frequency_histo.pdf")
@@ -64,4 +109,4 @@ f.suptitle('Qubit correlation - state-vector simulation')
 f.supxlabel('Qubit #')
 f.supylabel('Qubit #')
 plt.legend('Cij = COV(X_i,X_j)/(VAR(X_i)*VAR(X_j))^0.5', loc="upper right")
-plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/QFT/QFT_SV_correlation_heatmap.pdf")
+plt.savefig("c:/Users/tommy/OneDrive/Documenti/GitHub/QFT/QFT_SV_correlation_heatmap.pdf")"""

QFT/SV_QFT_sampling.py
@@ -8,44 +8,50 @@ import seaborn as sns
 from qibo.models import Circuit
 from qibo import gates
 
-maxqubit = 3       #33
-nsampling = 8    #100k ?
-
-N = maxqubit
-
-circ = Circuit(N)
-for i in range(N):
-
-    circ.add(gates.H(i))    
-    for j in range(i + 1, N):
-        theta = np.pi / 2 ** (j - i)    #rotation angle   
-        circ.add(gates.CU1(i, j, theta= theta))
-                
-for i in range(N // 2):
-    circ.add(gates.SWAP(i, N - i - 1))
-
-for i in range(N):
-    circ.add(gates.M(i))
+maxqubit = 33       #33
+nsampling = 10^5    #100k ?
 
 
-result_state = circ(nshots = nsampling)
+for N in range(1, maxqubit + 1):
 
-samples = result_state.samples(binary=True)
+    circ = Circuit(N)
 
-"""
-bigmatrix = np.zeros((nsampling, maxqubit), np.int8)
+    for i in range(N):
 
-row = 0
-for s in samples:
-    bigmatrix[row] = s
-    row += 1
+        circ.add(gates.H(i))    
+        for j in range(i + 1, N):
+            theta = np.pi / 2 ** (j - i)    #rotation angle   
+            circ.add(gates.CU1(i, j, theta= theta))
+                    
+    for s in range(N // 2):
+        circ.add(gates.SWAP(s, N - s - 1))
 
-print(bigmatrix)"""   
+    for m in range(N):
+        circ.add(gates.M(m))
 
-farray = np.sum(samples, axis = 0) / nsampling
-print(farray)
 
-rij = np.corrcoef(samples, rowvar= False)
-print(rij)
+    result_state = circ(nshots = nsampling)
 
-np.savetxt('SV_samples.txt', (farray, rij), delimiter=',')
+    samples = result_state.samples(binary=True)
+
+    """
+    bigmatrix = np.zeros((nsampling, maxqubit), np.int8)
+
+    row = 0
+    for s in samples:
+        bigmatrix[row] = s
+        row += 1
+
+    print(bigmatrix)"""   
+
+    farray = np.sum(samples, axis = 0) / nsampling
+    print(farray)
+
+    rij = np.corrcoef(samples, rowvar= False)
+    print(rij)
+
+    with open('/home/tommasogalletti/QFT/samples/SV__farrays.txt', mode='a') as file:
+        np.savetxt(file, farray, delimiter=',')
+
+    with open('/home/tommasogalletti/QFT/samples/SV__rs.txt', mode='a') as file:
+        np.savetxt(file, rij, delimiter=',')

QFT/SV_QFT_time_scaling.py
@@ -66,4 +66,4 @@ for n in range(maxqubit):
     meanprocesstime[n] = np.mean(singleprocesstime)
     processtimeerror[n] = np.mean(singleprocesstime)
 
-#np.savetxt('/home/tommasogalletti/QFT/time_arrays/SV_times.csv', (meantotaltime, totaltimeerror, meanprocesstime, processtimeerror), delimiter=',')
+np.savetxt('/home/tommasogalletti/QFT/time_arrays/SV_times.csv', (meantotaltime, totaltimeerror, meanprocesstime, processtimeerror), delimiter=',')

QFT/TN_QFT_sampling.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import quimb as qu
 import quimb.tensor as qtn
 
+
 def build_QFT(N, regs):
     for i in range(N):
         circ.apply_gate('H', regs[i])                               
@@ -19,30 +20,36 @@ def build_QFT(N, regs):
         circ.apply_gate('SWAP', regs[i], regs[N - i - 1])
 
 
-maxqubit = 6       #37
-nsampling = 20   #100k ?
- 
-N = maxqubit
+maxqubit = 33       #33
+nsampling = 10^5   #100k ?
 
-regs = list(range(N))
-circ = qtn.Circuit(N)
 
-build_QFT(N, regs)
+for N in range(1, maxqubit + 1):
+    regs = list(range(N))
+    circ = qtn.Circuit(N)
 
-bigmatrix = np.zeros((nsampling, N), np.int8)
+    build_QFT(N, regs)
 
-row = 0
-for b in circ.sample(nsampling):
-    
-    shot = list(b)
-    bigmatrix[row] = shot
-    row += 1
+    bigmatrix = np.zeros((nsampling, N), np.int8)
 
-farray = np.sum(bigmatrix, axis = 0) / nsampling
+    row = 0
+    for b in circ.sample(nsampling):
+        
+        shot = list(b)
+        bigmatrix[row] = shot
+        row += 1
 
-rij = np.corrcoef(bigmatrix, rowvar= False)
+    farray = np.sum(bigmatrix, axis = 0) / nsampling
 
-np.savetxt('TN_samples.txt', (farray, rij), delimiter=',')
+    rij = np.corrcoef(bigmatrix, rowvar= False)
+
+    with open('/home/tommasogalletti/QFT/samples/TN__farrays.txt', mode='a') as file:
+        np.savetxt(file, farray, delimiter=',')
+
+    with open('/home/tommasogalletti/QFT/samples/TN__rs.txt', mode='a') as file:
+        np.savetxt(file, rij, delimiter=',')
+
+
 
 """
 circ.psi.draw(color=['H', 'CU1', 'SWAP'])                   #circuit drawing - focus on gate types